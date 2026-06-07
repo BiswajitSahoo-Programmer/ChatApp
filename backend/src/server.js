cf2f9db47f215aa8c4cabb652e869aa5839de2a1
Biswajit Sahoo <bs600163@gmail.com>
2026-06-07 22:09:27 +0530

    Remove frontend dist serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,12 +21,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make for deployment
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
+//     app.get("*", (_, res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+//     })
+// }
 
 
 app.listen(PORT, () => {
