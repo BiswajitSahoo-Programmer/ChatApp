aaa2528b5d15f52c4fa67363c6785601d35d4d01
Biswajit Sahoo <bs600163@gmail.com>
2026-06-21 10:06:56 +0530

    Welcome email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
+import { ENV } from "./env.js";
 
 export const generateToken  = (userId, res) => {
-    const {JWT_SECRET,NODE_ENV} = process.env;
+    const {JWT_SECRET,NODE_ENV} = ENV;
     if (!JWT_SECRET) {
         throw new Error("JWT_SECRET is not defined in environment variables");
     }
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
         expiresIn: '7d',
     });
 
@@ -14,7 +15,7 @@ export const generateToken  = (userId, res) => {
         httpOnly: true, //prevents xss attack
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         sameSite: 'strict', //CSrf attac
-        secure: process.env.NODE_ENV === 'development' ? false : true, //only send cookie over https in production
+        secure: ENV.NODE_ENV === 'development' ? false : true, //only send cookie over https in production
     });
 
     return token;
